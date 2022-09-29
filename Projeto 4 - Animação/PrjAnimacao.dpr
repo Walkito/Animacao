ea45a97978809f6a9082ea9e518cc6c660bbfa38
Walker de Carvalho <56097547+Walkito@users.noreply.github.com>
2022-09-29 13:26:40 -0300

    Código Fonte
    Código Fonte do projeto que pode ser editado através do RAD Studio.

---
+++ Projeto 4 - Animação/PrjAnimacao.dpr
@@ -0,0 +1,14 @@
+program PrjAnimacao;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Animacao in 'Animacao.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
